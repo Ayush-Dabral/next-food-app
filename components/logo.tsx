b0c9126a13aff35efefa70af50d128458ecf1562
Ayush Dabral <ayush.dabral02@gmail.com>
2024-04-13 13:00:50 +0530

    dashboard updates added

---
+++ components/logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return ( 
+        <div className=" w-20 h-20 flex items-center justify-center rounded-full border-[1px] border-[#707070] bg-white text-4xl font-semibold text-logo md:w-36 md:h-36 md:text-6xl md:font-bold">
+            e!
+        </div>
+    );
+}
+ 
+export default Logo;
