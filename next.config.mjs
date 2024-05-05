06a3f2d49c4b2727a6cfb2db1a9bd98efd70b6f1
Ayush Dabral <ayush.dabral02@gmail.com>
2024-05-05 22:20:38 +0530

    3: changed configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: ['/assets/*'],
-      domains: ['b.zmtcdn.com'], // Add the hostname here
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'b.zmtcdn.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '/assets',
+        port: '',
+      }
+    ],
+  },
   };
   
   export default nextConfig;
